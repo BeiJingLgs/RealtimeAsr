ada12c76348c2e679811c3d907c6bc9454bf05ba
guhongbo <2978032936@qq.com>
2020-02-24 10:35:28 +0800

    将字段配置的dimen.xml

---
+++ app/src/main/java/com/hanvon/speech/realtime/services/AddCookiesInterceptor.java
@@ -0,0 +1,35 @@
+package com.hanvon.speech.realtime.services;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.alibaba.fastjson.JSONObject;
+import com.baidu.ai.speech.realtime.android.HvApplication;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class AddCookiesInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request.Builder builder = chain.request().newBuilder();
+        //String cookieStr = SPUtil.getData(Constant.SP.SP, Constant.SP.SESSION_ID, String.class, null);
+        //List<String> cookies = JSONObject.parseArray(cookieStr, String.class);
+       /* if (cookies != null) {
+            for (String cookie : cookies) {
+                builder.addHeader("Cookie", cookie);
+                Log.v("OkHttp", "Adding Header: " + cookie); // This is done so I know which headers are being added; this interceptor is used after the normal logging of OkHttp
+            }
+        }*/
+       if (TextUtils.isEmpty(HvApplication.SESSION))
+           return chain.proceed(builder.build());
+        builder.addHeader("Cookie", HvApplication.SESSION);
+        return chain.proceed(builder.build());
+    }
+}
+   
---
+++ app/src/main/java/com/hanvon/speech/realtime/services/ReceivedCookiesInterceptor.java
@@ -0,0 +1,36 @@
+package com.hanvon.speech.realtime.services;
+
+
+import android.util.Log;
+
+import com.alibaba.fastjson.JSONObject;
+import com.baidu.ai.speech.realtime.android.HvApplication;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+public class ReceivedCookiesInterceptor implements Interceptor {
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+
+        Response originalResponse = chain.proceed(chain.request());
+        if (!originalResponse.headers("Set-Cookie").isEmpty()) {
+            List<String> cookies = new ArrayList<>();
+            Log.e("cookies", " 1cookies.size: " + cookies.size());
+            for (String header : originalResponse.headers("Set-Cookie")) {
+                cookies.add(header);
+            }
+            HvApplication.SESSION = cookies.get(0);
+            Log.e("cookies", " 2cookies.: " + cookies.get(0));
+            Log.e("cookies", " 2cookies.size: " + cookies.size());
+            //String cookieStr = JSONObject.toJSONString(cookies);
+            //SPUtil.putData(HealthKeys.Constant.SP.SP, Constant.SP.SESSION_ID, cookieStr);
+        }
+
+        return originalResponse;
+    }
+}
